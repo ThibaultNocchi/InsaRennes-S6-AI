6975175d9a28c27a81bcc706643c463477cb77ed
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-01-29 16:10:41 +0100

    SlidingPuzzle
    Cleaned once again.

--- SlidingPuzzle/src/slidingpuzzle/Main.java
+++ SlidingPuzzle/src/slidingpuzzle/Main.java
@@ -4,15 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] puzzle = {{2,1,3},{4,6,5},{7,0,8}};
-        SlidingPuzzle sp = new SlidingPuzzle(puzzle);
-        sp.moveUp();
-        System.out.println(sp);
-        for(int i = 0; i < 3; ++i){
-            for(int j = 0; j < 3; ++j){
-                System.out.print(puzzle[i][j]+" ");
-            }
-        }
+        
 
     }
 }
