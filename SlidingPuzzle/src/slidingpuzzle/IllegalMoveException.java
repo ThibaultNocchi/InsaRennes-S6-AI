de699a160eaad030c98f6a0fdd7c0b4986cc4cf6
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-01-30 19:37:24 +0100

    SlidingPuzzle
    Commented and improved exception to standard.

--- SlidingPuzzle/src/slidingpuzzle/IllegalMoveException.java
+++ SlidingPuzzle/src/slidingpuzzle/IllegalMoveException.java
@@ -1,4 +1,25 @@
 package slidingpuzzle;
 
+/**
+ * Exception thrown when trying to move or get a move from a sliding puzzle if this move cannot be done.
+ * Used to easily detect if a move worked or not.
+ */
 public class IllegalMoveException extends RuntimeException {
+
+    public IllegalMoveException(){
+        super();
+    }
+
+    public IllegalMoveException(String message){
+        super(message);
+    }
+
+    public IllegalMoveException(Throwable cause){
+        super(cause);
+    }
+
+    public IllegalMoveException(String message, Throwable cause){
+        super(message,cause);
+    }
+
 }
