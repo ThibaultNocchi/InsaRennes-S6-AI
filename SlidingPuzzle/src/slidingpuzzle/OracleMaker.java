354469d5d7bd9d9e8e599c3f8ef679f85fdc8e7f
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-01-30 21:49:20 +0100

    SlidingPuzzle
    Commented OracleMaker.

--- SlidingPuzzle/src/slidingpuzzle/OracleMaker.java
+++ SlidingPuzzle/src/slidingpuzzle/OracleMaker.java
@@ -1,38 +1,60 @@
 package slidingpuzzle;
 
 import java.io.*;
-import java.nio.channels.Channels;
 import java.util.HashSet;
 import java.util.LinkedList;
 
+/**
+ * Builds an oracle for a given implementation of a SlidingPuzzle and a given root.
+ * The oracle will find the number of moves required to get to any position from the root position.
+ * Usually we give it the solution, so that we can deduce the number of moves required from any random set to solve it.
+ */
 public class OracleMaker {
 
+    /**
+     * Root puzzle.
+     */
     private final SlidingPuzzle root;
-    private LinkedList<SlidingPuzzle> toVisit;
+
+    /**
+     * Oracle with the different SlidingPuzzle and their distance (SlidingPuzzle.level) from the root.
+     */
     private HashSet<SlidingPuzzle> oracle;
 
+    /**
+     * Saves the root.
+     * @param root Root puzzle.
+     */
     public OracleMaker(SlidingPuzzle root){
         this.root = root;
+        this.oracle = new HashSet<>();
     }
 
+    /**
+     * Builds the oracle from the root.
+     * It follows a breadth first search algorithm.
+     * From the root, we first get all its different moves possible and add them to a queue.
+     * The root is then added to the oracle.
+     * We do it over again for every puzzle we can retrieve from the queue.
+     */
     public void buildOracle(){
 
-        this.toVisit = new LinkedList<>();
-        this.oracle = new HashSet<>();
-        this.toVisit.add(this.root);
+        LinkedList<SlidingPuzzle> toVisit = new LinkedList<>();
+        this.oracle.clear();
+        toVisit.add(this.root);
         int currentLevel = this.root.getLevel();
 
-        while(this.toVisit.size() > 0){
+        while(toVisit.size() > 0){
 
-            SlidingPuzzle current = this.toVisit.poll();
+            SlidingPuzzle current = toVisit.poll();
 
-            if(currentLevel != current.getLevel()){
+            if(currentLevel != current.getLevel()){     // Outputs the new level of search when we change it.
                 currentLevel = current.getLevel();
                 System.out.println(currentLevel);
             }
 
             for(SlidingPuzzle move : current.getLegalMoves()){
-                if(!this.toVisit.contains(move) && !this.oracle.contains(move)) this.toVisit.add(move);
+                if(!toVisit.contains(move) && !this.oracle.contains(move)) toVisit.add(move);
             }
 
             this.oracle.add(current);
@@ -41,6 +63,11 @@ public class OracleMaker {
 
     }
 
+    /**
+     * Saves a previously calculated oracle to a file.
+     * @param path Path to the file.
+     * @throws IOException
+     */
     public void saveOracle(String path) throws IOException {
 
         File file = new File(path);
@@ -52,6 +79,10 @@ public class OracleMaker {
 
     }
 
+    /**
+     * Gets the oracle.
+     * @return The oracle.
+     */
     public HashSet<SlidingPuzzle> getOracle(){
         return this.oracle;
     }
