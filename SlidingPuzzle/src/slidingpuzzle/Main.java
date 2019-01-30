b2caf70f7bf538c26e43ac73900e13a79e37fccc
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-01-30 21:56:08 +0100

    SlidingPuzzle
    Commented OracleSolver.

--- SlidingPuzzle/src/slidingpuzzle/Main.java
+++ SlidingPuzzle/src/slidingpuzzle/Main.java
@@ -1,6 +1,7 @@
 package slidingpuzzle;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Objects;
 
@@ -8,12 +9,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        OracleMaker oracle = new OracleMaker(new SlidingPuzzleInt(3));
-        oracle.buildOracle();
-        oracle.saveOracle("");
+//        OracleMaker oracle = new OracleMaker(new SlidingPuzzleInt(3));
+//        oracle.buildOracle();
+//        oracle.saveOracle("oracle3x3.txt");
 
-        OracleSolver oracleSolve = new OracleSolver("");
-        oracleSolve.solve(new SlidingPuzzleInt("0 6 4 5 2 1 3 7 8 0"));
+        OracleSolver oracleSolve = new OracleSolver("oracle3x3.txt");
+        ArrayList<SlidingPuzzle> solutions = oracleSolve.solve(new SlidingPuzzleInt("0 6 4 5 2 1 3 7 8 0"));
+        for(SlidingPuzzle solution : solutions){
+            System.out.println(solution);
+        }
 
     }
 }
--- SlidingPuzzle/src/slidingpuzzle/OracleSolver.java
+++ SlidingPuzzle/src/slidingpuzzle/OracleSolver.java
@@ -4,12 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Solves a SlidingPuzzle with a previously generated oracle file.
+ */
 public class OracleSolver {
 
+    /**
+     * Parsed oracle with SlidingPuzzle as key and its level as value.
+     */
     private HashMap<SlidingPuzzle, Integer> oracle;
 
+    /**
+     * Parses a file with the oracle locally.
+     * @param path Path to the oracle.
+     * @throws IOException
+     */
     public OracleSolver(String path) throws IOException {
 
         this.oracle = new HashMap<>();
@@ -25,21 +37,35 @@ public class OracleSolver {
 
     }
 
-    public void solve(SlidingPuzzle slidingpuzzle){
+    /**
+     * Gets the path to the solution of the oracle from the given puzzle.
+     * @param slidingpuzzle Puzzle to start from.
+     * @return List of puzzles to go through (effectively are moves) to reach the solution of the oracle.
+     */
+    public ArrayList<SlidingPuzzle> solve(SlidingPuzzle slidingpuzzle){
 
         SlidingPuzzle current = slidingpuzzle;
         current.setLevel(this.getLevelOfPuzzle(current));
+        ArrayList<SlidingPuzzle> solutions = new ArrayList<>();
+        solutions.add(current);
 
         while(current.getLevel() > 0){
             for(SlidingPuzzle move : current.getLegalMoves()){
                 if(this.getLevelOfPuzzle(move) < current.getLevel()) current = move;
             }
             current.setLevel(this.getLevelOfPuzzle(current));
-            System.out.println(current);
+            solutions.add(current);
         }
+
+        return solutions;
 
     }
 
+    /**
+     * Gets the level of a puzzle in the oracle.
+     * @param slidingPuzzle Puzzle to get the level.
+     * @return Level of the puzzle.
+     */
     public int getLevelOfPuzzle(SlidingPuzzle slidingPuzzle){
         return this.oracle.get(slidingPuzzle);
     }
