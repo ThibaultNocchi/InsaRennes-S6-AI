857a00345ff0bed1df0a4b117f1cc73237316c9c
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-01-30 22:03:06 +0100

    SlidingPuzzle
    Commented Main app.

--- SlidingPuzzle/src/slidingpuzzle/Main.java
+++ SlidingPuzzle/src/slidingpuzzle/Main.java
@@ -11,14 +11,14 @@ public class Main {
 
         String filePath = "oracle2x2.txt";
 
-        OracleMaker oracle = new OracleMaker(new SlidingPuzzleInt(2));
-        oracle.buildOracle();
-        oracle.saveOracle(filePath);
+        OracleMaker oracle = new OracleMaker(new SlidingPuzzleInt(2));  // Creates an oracle with a goal being a 2x2 solution puzzle.
+        oracle.buildOracle();   // Generates the oracle.
+        oracle.saveOracle(filePath);    // Saves it to a file.
 
-        OracleSolver oracleSolve = new OracleSolver(filePath);
-        ArrayList<SlidingPuzzle> solutions = oracleSolve.solve(new SlidingPuzzleInt("0 0 3 1 2"));
+        OracleSolver oracleSolve = new OracleSolver(filePath);  // Loads an oracle from the file.
+        ArrayList<SlidingPuzzle> solutions = oracleSolve.solve(new SlidingPuzzleInt("0 0 3 2 1"));  // Gets the solution of a puzzle 0 3 2 1. (First 0 is the level of the puzzle, which doesn't matter when solving it.)
         for(SlidingPuzzle solution : solutions){
-            System.out.println(solution);
+            System.out.println(solution);   // Prints the path to the solution.
         }
 
     }
